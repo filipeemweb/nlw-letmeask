4c08f72e7142714b79f87be8df199cb4667deb56
Filipe Lacerda <filipeem.web@gmail.com>
2021-06-24 04:46:55 -0300

    Finaliza e implementa algumas das funcionalidades junto ao BD das salas de perguntas

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,85 @@
+import { useHistory, useParams } from "react-router-dom";
+
+import logoImg from "../assets/images/logo.svg";
+import deleteImg from "../assets/images/delete.svg";
+
+import { Button } from "../components/Button";
+import { Question } from "../components/Question";
+import { RoomCode } from "../components/RoomCode";
+// import { useAuth } from "../hooks/useAuth";
+import { useRoom } from "../hooks/useRoom";
+
+import "../styles/room.scss";
+import { database } from "../services/firebase";
+
+type RoomParams = {
+  id: string;
+};
+
+export function AdminRoom() {
+  // const { user } = useAuth();
+  const history = useHistory();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const { questions, title } = useRoom(roomId);
+
+  async function handleEndRoom() {
+    if (window.confirm("Tem certeza que você deseja finalizar esta sala?")) {
+      await database.ref(`rooms/${roomId}`).update({
+        closedAt: new Date(),
+      });
+
+      history.push("/");
+    }
+  }
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined onClick={handleEndRoom}>
+              Encerrar sala
+            </Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              >
+                <button>
+                  <img
+                    src={deleteImg}
+                    alt="Remove question"
+                    onClick={() => handleDeleteQuestion(question.id)}
+                  />
+                </button>
+              </Question>
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -34,7 +34,12 @@ export function Home() {
     const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
     if (!roomRef.exists()) {
-      alert("Room does not exists.");
+      alert("Esta sala não existe.");
+      return;
+    }
+
+    if (roomRef.val().closedAt) {
+      alert("Esta sala já foi encerrada.");
       return;
     }
 
